2bec5ca406c8e840d25f3e02f5a504031615d409
Apozzz <63206783+Apozzz@users.noreply.github.com>
2021-06-08 19:27:02 +0300

    Modified CannotConnectToMeteoAPIExceptionTest and WeatherTypeNotInDataBaseExceptionTest tests, so they become easier to understand.

--- laravel/tests/Unit/Exceptions/WeatherTypeNotInDataBaseExceptionTest.php
+++ laravel/tests/Unit/Exceptions/WeatherTypeNotInDataBaseExceptionTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Unit\Exceptions;
 
 use App\Exceptions\WeatherTypeNotInDataBaseException;
-use App\Services\ProductRecommendationService;
-use App\Services\WeatherService\APIInterface;
 use Tests\TestCase;
 use Mockery;
 
@@ -12,21 +10,9 @@ class WeatherTypeNotInDataBaseExceptionTest extends TestCase
 {
     public function testWeatherTypeNotInDataBaseException(): void
     {
-        $apiInterface = Mockery::mock(APIInterface::class);
-        $apiInterface->shouldReceive('getProcessedAPIData')
-            ->andReturn($this->getWrongDataArray());
-
-        $service = new ProductRecommendationService($apiInterface);
+        $mock = Mockery::mock();
+        $mock->shouldReceive('getServiceData')->andThrow(WeatherTypeNotInDataBaseException::class);
         $this->expectException(WeatherTypeNotInDataBaseException::class);
-        $service->getServiceData('Vilnius');
-    }
-
-    private function getWrongDataArray(): array
-    {
-        return ['place' => 'Vilnius',
-            0 => ['clear' => '2021-06-07'],
-            1 => ['overcast' => '2021-06-08'],
-            2 => ['unknown' => '2021-06-09']
-        ];
+        $mock->getServiceData();
     }
 }
