3085a1d91d8eb212c0c748a0704efc2e34816ab6
Apozzz <63206783+Apozzz@users.noreply.github.com>
2021-06-08 13:34:27 +0300

    Created factory, that populates Products table with random products, generated using faker. Modified databaseSeeder file, so it populates weathers, products and weather_product tables with data. Weathers data is hardcoded, other two's tables randomly generated.

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Weather;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Product::factory(13)->create();
+
+        // Seeding Weathers table with predefined weather types.
+        foreach ($this->getWeatherData() as $weather) {
+            Weather::create(['name' => $weather]);
+        }
+
+        // Seeding many-to-many relation table. Every product gets 1-3 random weather types
+        // assigned.
+        $weathers = Weather::all();
+        Product::all()->each(function ($product) use ($weathers) {
+            $product->weathers()->attach(
+                $weathers->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
+    }
+    protected function getWeatherData(): array
+    {
+        return [
+            0 => 'clear',
+            1 => 'isolated-clouds',
+            2 => 'scattered-clouds',
+            3 => 'overcast',
+            4 => 'light-rain',
+            5 => 'moderate-rain',
+            6 => 'heavy-rain',
+            7 => 'sleet',
+            8 => 'light-snow',
+            9 => 'moderate-snow',
+            10 => 'heavy-snow',
+            11 => 'fog',
+            12 => 'na'
+        ];
     }
 }
